679df1c1a788aeb60cdde2a5b7bc4234c15fa5b3
workerVA <64370427+workerVA@users.noreply.github.com>
2021-08-11 18:20:12 +0300

    Update run.py

--- Test1/dockre_blk/pth/run.py
+++ Test1/dockre_blk/pth/run.py
@@ -1,2 +1,55 @@
-with open('/usr/share/tmp','w') as f:
-    f.write('123123')
+import sys
+import os
+from jnpr.junos import Device
+from jnpr.junos.utils.config import Config
+from jnpr.junos.exception import ConnectError
+
+import threading
+import signal
+
+def checkAddrMask(ipMask):
+    if "/" in ipMask and len(ipMask.split("/")) and ipMask.split("/")[1].isdigit():
+            if int(ipMask.split("/")[1])>=0 and int(ipMask.split("/")[1])<33:
+                if "." in ipMask and len(ipMask.split("."))==4:
+                    ipDigit = ipMask.split("/")[0].split(".")
+                    for xTmp in ipDigit:
+                        if not xTmp.isdigit():
+                            return False
+                        if int(xTmp) < 0 or int(xTmp) > 255:
+                            return False
+                    return True
+    return False
+
+def threadWorks():
+    if os.path.exists('/usr/share/ipTmp'):
+    # how insert host pass ....
+    #    try:
+    #        dev = Device(host=host_name, user=user_name, password=passw_name)
+    #        dev.open()
+    #        dev.timeout = 300
+    #    except ConnectError as err:
+    #        print ("Cannot connect to device: {0}".format(err))
+    #        return
+    
+        if True:#after del
+    #    with Config(dev, mode='private') as cu:  
+            addrList = {}
+            with open('/usr/share/ipTmp','a+') as f:
+                addrList = f.read().splitlines()
+                f.truncate()
+    
+            for ipAddr in addrList:
+                #dell this 3 lines
+                if checkAddrMask(ipAddr):
+                    with open('/usr/share/tmp','w') as f:
+                        f.write(ipAddr)
+    #                cu.load('set firewall filter F-EGRESS term DENY-ATTACKERS from source-address '+ ipAddr, format='set')
+    #        if cu.diff():
+    #            cu.commit()
+    #        cu.commit()
+    #    dev.close()
+							#change time to 1800.0
+    timer = threading.Timer(180.0, threadWorks)
+    timer.start()
+
+threadWorks()
